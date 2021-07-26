fdff0b257d810f30b4cbfb54105c9cbdff008ffb
Cihan <cihangonen35@gmail.com>
2021-07-26 14:44:24 +0300

    experience responsiveness

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -2,63 +2,56 @@ import './Experience.css'
 
 const Experience = ()=>{
   return(
-    <div className="flex h-full flex-col lg:justify-around exp-wrap">
-      <div className="h-80 lg:h-24 flex pt-5 lg:pt-0 flex-col lg:flex-row items-center gap-5 lg:gap-10 px-9">
-        <h2 className="text-center text-base md:text-lg lg:text-start lg:text-xl font-bold">
+    <div className="h-full p-5">
+      <div className="">
+        <h1 className="font-bold text-xl">
           Experience
-        </h2>
-        <div className="flex flex-col h-full justify-center gap-5">
-          <div>
-            <h3 className="text-sm font-semibold xl:font-normal xl:text-lg xl:text-xl">
-             -  Grafiket Adworks / October - December 2020
-            </h3>
-            <h4 className="text-sm font-semibold xl:font-normal xl:text-lg">
-              Frontend Develeoper
-            </h4>
-          </div>
-          <p className="text-sm xl:text-base">
-            Worked with designers and I made their designs alive using html and css.
-            Also worked with a back-end developer. I learned to turn design files to working web pages.
-          </p>
+        </h1>
+        <div className="pt-1 flex flex-col xl:flex-row gap-5">
+          <h2 className="text-lg">
+            - Grafiket Adworks / October - December 2020
+          </h2>
+          <h3 className="font-bold text-lg">
+            Frontend Develeoper
+          </h3>
         </div>
+        <p className="pt-3">
+          Worked with designers and I made their designs alive using html and css. Also worked with a back-end developer. I learned to turn design files to working web pages.
+        </p>
       </div>
-      <div className="h-1 rounded-md w-11/12 mx-auto my-hr">
+      <div className="h-1 mt-5 rounded-md w-11/12 mx-auto my-hr">
       </div>
-      <div className="h-96 lg:h-56 flex pt-5 lg:pt-0 flex-col lg:flex-row items-center gap-5 lg:gap-10 px-9">
-      <h2 className="text-center font-extrabold text-lg lg:text-start lg:text-xl lg:font-bold">
-          Education
-      </h2>
-      <div className="flex flex-col gap-5 h-full justify-center">
-        <div className="flex flex-col h-full justify-center gap-5">
-          <div>
-            <h3 className="text-sm font-semibold xl:font-normal xl:text-xl">
-             -  Firat University / 2017 - 2018 
-            </h3>
-            <h4 className="text-sm font-semibold xl:font-normal xl:text-lg">
+      <div className="pt-5">
+        <div className="">
+          <h1 className="font-bold text-xl">
+            Education
+          </h1>
+          <div className="pt-1 flex flex-col xl:flex-row gap-5">
+            <h2 className="text-lg">
+              - Firat University / 2017 - 2018
+            </h2>
+            <h3 className="font-bold text-lg">
               English Preparation - GPA (3.5/4)
-            </h4>
+            </h3> 
           </div>
-          <p className="text-sm xl:text-base">
-            I learned algorithmic thinking and solved many problems using Java and C#.
-            I also learned and used agile and scrum methodologies in my school projects.
+          <p className="pt-3">
+            I learned algorithmic thinking and solved many problems using Java and C#. I also learned and used agile and scrum methodologies in my school projects.
           </p>
         </div>
-        <div  className="flex flex-col h-full justify-center gap-5">
-          <div>
-            <h3 className="text-sm font-semibold xl:font-normal xl:text-xl">
-             -  Firat University / 2018 - Present (4/4)
+        <div className="mt-5">
+          <div className="pt-1 flex flex-col xl:flex-row gap-5">
+            <h2 className="text-lg">
+            - Firat University / 2018 - Present (4/4)
+            </h2>
+            <h3 className="font-bold text-lg">
+            Software Engineering - Bachelor's Degree - GPA (3.48)
             </h3>
-            <h4 className="text-sm font-semibold xl:font-normal xl:text-lg">
-              Software Engineering - Bachelor's Degree - GPA (3.48)
-            </h4>
           </div>
-          <p className="text-sm xl:text-base">
-            I studied just English for 2 semesters. I discussed subjects in english and wrote many essays
-            and did many presentations.
+          <p className="pt-3">
+             I studied just English for 2 semesters. I discussed subjects in english and wrote many essays and did many presentations.
           </p>
         </div>
       </div>
-    </div>
   </div>
   )
 }
